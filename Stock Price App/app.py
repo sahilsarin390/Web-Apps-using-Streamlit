75f9856fd536a0ce7a7121a4e4e83d847c824ad2
Sahil <sahilsarin390@gmail.com>
2022-01-20 17:26:57 +0530

    Enhanced Data

--- Stock Price App/app.py
+++ Stock Price App/app.py
@@ -3,17 +3,51 @@ import streamlit as st
 
 st.write("""
 # Simple Stock Price App
-Shown are the stock closing price and volume of Google!
 """)
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
 # define the ticker symbol
-tickerSymbol = 'GOOGL'
+tickerSymbol = 'MSFT'
 # get data on this ticker
 tickerData = yf.Ticker(tickerSymbol)
 # get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
+tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2021-5-31')
 
+st.write("""
+#### Stock Opening Price!
+""")
+st.line_chart(tickerDf.Open)
+
+st.write("""
+#### Stock Closing Price!
+""")
 st.line_chart(tickerDf.Close)
+
+st.write("""
+#### Highest price the stock achieved that day!
+""")
+st.line_chart(tickerDf.High)
+
+st.write("""
+#### Lowest price the stock achieved that day!
+""")
+st.line_chart(tickerDf.Low)
+
+st.write("""
+#### Stock Volume!
+""")
 st.line_chart(tickerDf.Volume)
+
+st.write("""
+##### Some information about the company:
+""")
+tickerData.info
+
+st.write("""
+##### Upcoming earnings events regarding the company:
+""")
+tickerData.calendar
+
+st.write("""
+##### Recommendations from analysts:
+""")
+tickerData.recommendations
